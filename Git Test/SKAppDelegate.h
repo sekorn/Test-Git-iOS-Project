4f4aa23a2d7329760f80ccface59615a550572fc
Scott Kornblatt <sekorn@scotts-imac.home>
2014-03-10 21:56:02 -0400

    Initial Commit

---
+++ Git Test/SKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SKAppDelegate.h
+//  Git Test
+//
+//  Created by Scott Kornblatt on 3/10/14.
+//  Copyright (c) 2014 Scott Kornblatt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/SKViewController.h
@@ -0,0 +1,13 @@
+//
+//  SKViewController.h
+//  Git Test
+//
+//  Created by Scott Kornblatt on 3/10/14.
+//  Copyright (c) 2014 Scott Kornblatt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKViewController : UIViewController
+
+@end
