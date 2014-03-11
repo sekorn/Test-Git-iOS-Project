9c82e33ad204f7547e473ac55aa74ba66d88f83c
Scott Kornblatt <sekorn@scotts-imac.home>
2014-03-10 22:12:31 -0400

    first comment commit

---
+++ Git Test/SKTest.h
@@ -0,0 +1,13 @@
+//
+//  SKTest.h
+//  Git Test
+//
+//  Created by Scott Kornblatt on 3/10/14.
+//  Copyright (c) 2014 Scott Kornblatt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SKTest : NSObject
+
+@end
---
+++ Git Test/SKTest.m
@@ -0,0 +1,13 @@
+//
+//  SKTest.m
+//  Git Test
+//
+//  Created by Scott Kornblatt on 3/10/14.
+//  Copyright (c) 2014 Scott Kornblatt. All rights reserved.
+//
+
+#import "SKTest.h"
+
+@implementation SKTest
+
+@end
